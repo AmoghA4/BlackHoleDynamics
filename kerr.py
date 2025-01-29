4e358b5aeab1b718afb7ec6a419333d5d7fb0af0
Amogh A <115649977+AmoghA4@users.noreply.github.com>
2025-01-29 21:39:26 +0530

    kerr_v2.py

--- kerr.py
+++ kerr.py
@@ -5,16 +5,15 @@ from math import cos, pi
 
 # Number of particles
 num_particles = 20
-
+a = 0.999
+p = 3
+e = 0.4
+x = cos(pi/6)
+i = 0.1
 # Generate orbits with slight variations in initial phases
-for i in range(num_particles):
-    # Slight variation in initial phases
-    initial_phases = {
-        'psi_r': 1e-2 * i + 0.0,
-        'chi': 0.0,
-        'phi': 0.0
-    }
-    # Create the stable orbit
-    orbit = kg.StableOrbit(0.99, 3, 0.4, cos(pi/7), initial_phases=initial_phases)
-
-fig, ax = orbit.plot(0,10)
+for i in range(2):
+    orbit = kg.StableOrbit(a, p, e, x, initial_phases=(0, 0 + i, 0, 0))
+    i = i + 1
+    fig, ax = orbit.plot(0,10)
+    
+plt.show()
